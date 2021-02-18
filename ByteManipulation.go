47fb940bee88619d192f4d61439252987f715ea2
Jad Wahab <j.wahab@nchain.com>
2021-02-18 14:54:11 +0000

    rm code in go-bt

--- ByteManipulation.go
+++ ByteManipulation.go
@@ -2,7 +2,6 @@ package bc
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/hex"
 	"log"
 	"sort"
@@ -39,18 +38,6 @@ func SortByteArrays(src [][]byte) [][]byte {
 	return sorted
 }
 
-// ReverseBytes reverses the bytes (little endian/big endian).
-// This is used when computing merkle trees in Bitcoin, for example.
-func ReverseBytes(a []byte) []byte {
-	tmp := make([]byte, len(a))
-	copy(tmp, a)
-
-	for i, j := 0, len(a)-1; i < j; i, j = i+1, j-1 {
-		tmp[i], tmp[j] = tmp[j], tmp[i]
-	}
-	return tmp
-}
-
 // ReverseHexString reverses the hex string (little endian/big endian).
 // This is used when computing merkle trees in Bitcoin, for example.
 func ReverseHexString(hex string) string {
@@ -64,16 +51,6 @@ func ReverseHexString(hex string) string {
 		res += hex[i-2 : i]
 	}
 	return res
-}
-
-// GetLittleEndianBytes returns a byte array in little endian from an unsigned integer of 32 bytes.
-func GetLittleEndianBytes(v uint32, l uint32) []byte {
-	// TODO: is v hex encoded?
-	buf := make([]byte, l)
-
-	binary.LittleEndian.PutUint32(buf, v)
-
-	return buf
 }
 
 // Equals checks if two byte arrays are equal.
