0ddbad3d50a4ca19c4f9f788c74d306b8249e1b4
Jad Wahab <j.wahab@nchain.com>
2021-08-13 12:57:05 +0100

    refactor: update tests

--- blockheader_test.go
+++ blockheader_test.go
@@ -1,15 +1,13 @@
 package bc_test
 
 import (
-	"encoding/hex"
 	"testing"
 
 	"github.com/libsv/go-bc"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncodeBlockHeader(t *testing.T) {
-	headerBytes := "0000002074a17794e7890e9124d87e122b7f67b9d707dcb6c5b9d542b22eff3d13054678e9d8afa92026c2c0873524b18cbf2479720a8471952770c847d9ec8e1e939dfc1f593460ffff7f2000000000"
+func TestNewBlockHeader(t *testing.T) {
 	ebh := &bc.BlockHeader{
 		Version:        536870912,
 		HashPrevBlock:  "784605133dff2eb242d5b9c5b6dc07d7b9677f2b127ed824910e89e79477a174",
@@ -19,13 +17,16 @@ func TestEncodeBlockHeader(t *testing.T) {
 		Nonce:          0,
 	}
 
+	headerBytes := "0000002074a17794e7890e9124d87e122b7f67b9d707dcb6c5b9d542b22eff3d13054678e9d8afa92026c2c0873524b18cbf2479720a8471952770c847d9ec8e1e939dfc1f593460ffff7f2000000000"
 	bh, err := bc.NewBlockHeaderFromStr(headerBytes)
 
 	assert.NoError(t, err)
 	assert.Equal(t, ebh, bh)
 }
 
-func TestDecodeBlockHeader(t *testing.T) {
+func TestBlockHeaderString(t *testing.T) {
+	expectedHeader := "00000020fb9eacea87c1cc294a4f1633a45b9bfb21cf9878b439c6138d96b8ca3a856e3a37307cd123724eaa4ade23d29feea1358458d5c110275b6cca4e2b79cd14d98e39573460ffff7f2000000000"
+
 	bh := &bc.BlockHeader{
 		Version:        536870912,
 		HashPrevBlock:  "3a6e853acab8968d13c639b47898cf21fb9b5ba433164f4a29ccc187eaac9efb",
@@ -34,10 +35,7 @@ func TestDecodeBlockHeader(t *testing.T) {
 		Bits:           "207fffff",
 		Nonce:          0,
 	}
-	expectedHeader := "00000020fb9eacea87c1cc294a4f1633a45b9bfb21cf9878b439c6138d96b8ca3a856e3a37307cd123724eaa4ade23d29feea1358458d5c110275b6cca4e2b79cd14d98e39573460ffff7f2000000000"
-
-	headerBytes, err := bh.Bytes()
-	header := hex.EncodeToString(headerBytes)
+	header, err := bh.String()
 
 	assert.NoError(t, err)
 	assert.Equal(t, expectedHeader, header)
@@ -47,6 +45,7 @@ func TestExtractMerkleRootFromBlockHeader(t *testing.T) {
 	header := "000000208e33a53195acad0ab42ddbdbe3e4d9ca081332e5b01a62e340dbd8167d1a787b702f61bb913ac2063e0f2aed6d933d3386234da5c8eb9e30e498efd25fb7cb96fff12c60ffff7f2001000000"
 
 	merkleRoot, err := bc.ExtractMerkleRootFromBlockHeader(header)
+
 	assert.NoError(t, err)
 	assert.Equal(t, merkleRoot, "96cbb75fd2ef98e4309eebc8a54d2386333d936ded2a0f3e06c23a91bb612f70")
 }
