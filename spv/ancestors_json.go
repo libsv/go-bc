fef90f8cf82193f1d7877e28e59bbcc2acafac43
Darren Kellenschwiler <d.kellenschwiler@nchain.com>
2021-11-24 17:32:50 -0600

    start of JSON to Bytes and back again
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@nchain.com>

--- spv/ancestors_json.go
+++ spv/ancestors_json.go
@@ -10,11 +10,60 @@ import (
 // AncestryJSON is a spec at https://tsc.bitcoinassociation.net/standards/spv-envelope/ eventually.
 type AncestryJSON struct {
 	PaymentTx string `json:"paymentTx,omitempty"`
+	Depth     uint64 `json:"depth,omitempty"`
 	Ancestors []struct {
 		RawTx         string            `json:"hex,omitempty"`
 		Proof         *bc.MerkleProof   `json:"proof,omitempty"`
 		MapiResponses []bc.MapiCallback `json:"mapiResponses,omitempty"`
 	} `json:"ancestors,omitempty"`
+}
+
+func AncestoryJSONFromBytes(b []byte) (*AncestryJSON, error) {
+	ancestry, err := NewAncestryFromBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	ancestors := make([]struct{
+		RawTx         string
+		Proof         *bc.MerkleProof
+		MapiResponses []bc.MapiCallback
+	})
+	for ancestorID, ancestor := range ancestry.Ancestors {
+		rawTx := ancestor.Tx.String()
+		proof := &bc.MerkleProof{}
+		mpb, err := parseBinaryMerkleProof(ancestor.Proof)
+		if err != nil {
+			return nil, err
+		}
+		if mpb.flags | targetTypeFlags == 0 {
+			"header"
+		}
+		mp := &bc.MerkleProof{
+			Index:      mpb.index,
+			TxOrID:     mpb.txOrID,
+			Target:     mpb.target,
+			Nodes:      mpb.nodes,
+			TargetType: target,
+			ProofType:  "",
+			Composite:  false,
+		}
+		a := struct{
+			RawTx         string
+			Proof         *bc.MerkleProof
+			MapiResponses []bc.MapiCallback
+		}{ 
+			RawTx: rawTx, 
+			Proof: &bc.MerkleProof{}, 
+			MapiResponses: ancestor.MapiResponses
+		}
+		ancestors = append(ancestors, a)
+	}
+	j := &AncestryJSON{
+		PaymentTx: ancestry.PaymentTx.String(),
+		Depth:     0,
+		Ancestors: ,
+	}
+	return j, nil
 }
 
 // Bytes takes an AncestryJSON and returns the serialised bytes.
