6041c734937d34980cd4645407a2c1258f4301b6
Darren Kellenschwiler <d.kellenschwiler@nchain.com>
2021-11-30 09:36:47 -0600

    returning json parsing for the nested doc for debugging purposes.
    Signed-off-by: Darren Kellenschwiler <d.kellenschwiler@nchain.com>

--- spv/envelope.go
+++ spv/envelope.go
@@ -13,11 +13,11 @@ import (
 // Envelope is a struct which contains all information needed for a transaction to be verified.
 //
 type Envelope struct {
-	TxID          string
-	RawTx         string
-	Proof         *bc.MerkleProof
-	MapiResponses []bc.MapiCallback
-	Parents       map[string]*Envelope
+	TxID          string               `json:"txid,omitempty"`
+	RawTx         string               `json:"rawTx,omitempty"`
+	Proof         *bc.MerkleProof      `json:"proof,omitempty"`
+	MapiResponses []bc.MapiCallback    `json:"mapiResponses,omitempty"`
+	Parents       map[string]*Envelope `json:"parents,omitempty"`
 }
 
 // IsAnchored returns true if the envelope is the anchor tx.
